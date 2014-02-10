3a5b76e4553065d656d94e15a95dc7fb0f597fe2
Дмитрий Шульгачик <shulgachik@gmail.com>
2014-02-10 08:44:19 +0400

    new version

--- install/_version.php
+++ install/_version.php
@@ -1,6 +1,6 @@
 <?
 $arModuleVersion = array(
-	"VERSION" => "0.0.3",
-	"VERSION_DATE" => "2014-01-27 14:00:00",
+	"VERSION" => "0.0.4",
+	"VERSION_DATE" => "2014-01-30 08:58:47"
 );
 ?>
--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?
 $arModuleVersion = array(
-	"VERSION" => "0.0.4",
-	"VERSION_DATE" => "2014-01-30 08:58:47"
+	"VERSION" => "0.0.5",
+	"VERSION_DATE" => "2014-02-10 08:27:44"
 );
 ?>
